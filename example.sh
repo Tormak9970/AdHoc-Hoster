659e6da1b2335feca0106edf17d008594b47ef28
Tormak <63308171+Tormak9970@users.noreply.github.com>
2024-04-07 15:53:35 -0400

    chore: added disconnect example

--- example.sh
+++ example.sh
@@ -3,4 +3,9 @@
 NAME="Steamdeck"
 PASSWORD="test1234"
 
+# Starting the network:
 sudo nmcli dev wifi hotspot ifname wlan0 ssid "${NAME}" password "${PASSWORD}"
+
+
+# killing the network:
+sudo nmcli connection down "${NAME}"
